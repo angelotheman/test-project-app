02fc2309b1ddc9244043bbf1f8b10a3b8fb21d10
angelotheman <ktwum721@gmail.com>
2023-06-05 12:42:07 +0000

    New effects

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models;
 
@@ -7,9 +8,11 @@ public class Movie
     public int Id { get; set; }
     public string? Title { get; set; }
 
+    [Display(Name = "Date Released")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
     [Range(10, 1000, ErrorMessage = "The price should be between 10 and 1000")]
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 }
